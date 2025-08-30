7d7a0d1219c6c211e99e527013231ea841fc0816
Tushar Kamble <kambletushar025@gmail.com>
2025-08-31 02:51:57 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <h1>Tic Tac Toe</h1>
+    </React.Fragment>
+  )
+}
+
+export default App
